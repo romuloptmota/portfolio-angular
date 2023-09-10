1c52071d5edbe44850211b9ba23dda520b2b5eec
Romulo P T Mota <romuloptmota@gmail.com>
2023-09-10 14:22:02 -0300

    Criado menu mini e menu compacto

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
-export class HeaderComponent {
+export class HeaderComponent{
+
+  public menuCompleto: boolean = true;
+  public menuMini: boolean = false;
+  public menuCompacto: boolean = false;
+
+  @HostListener('window:resize')
+  onsize(): void{
+    if(window.innerWidth > 690){
+      this.menuCompleto = true;
+      this.menuMini = false;
+      this.menuCompacto = false;
+    } else if(window.innerWidth > 370) {
+      this.menuCompleto = false;
+      this.menuMini = true;
+      this.menuCompacto = false;
+    } else{
+      this.menuCompleto = false;
+      this.menuMini = false;
+      this.menuCompacto = true;
+    }
+  }
+
 
 }
