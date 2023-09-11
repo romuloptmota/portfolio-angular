99375d63949afe85c607f40bc2d2687d648c6221
Romulo P T Mota <romuloptmota@gmail.com>
2023-09-10 21:31:32 -0300

    Criado pagina sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page/page.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './page.component.html',
+  styleUrls: ['./page.component.less']
+})
+export class PageComponent implements OnInit{
+
+  public condicaoDesenvolvedor: boolean = true;
+  public condicaoFullStack: boolean = false;
+
+  ngOnInit() {
+    setInterval(() => {
+      if(this.condicaoDesenvolvedor){
+        this.condicaoDesenvolvedor = false;
+        this.condicaoFullStack = true;
+
+      }else{
+        this.condicaoDesenvolvedor = true;
+        this.condicaoFullStack = false;
+      }
+    }, 5000)
+  }
+
+}
