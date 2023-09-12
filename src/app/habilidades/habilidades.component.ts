7da37e7359594bdcfb9281d6f30b09b702f562d7
Romulo P T Mota <romuloptmota@gmail.com>
2023-09-11 23:26:41 -0300

    Corrigido erro em metodo de interatividade na template habilidade após criar simulação de banco de dados

--- src/app/habilidades/habilidades.component.ts
+++ src/app/habilidades/habilidades.component.ts
@@ -11,4 +11,16 @@ export class HabilidadesComponent{
   //Para setar informação no template simulando banco de dados
   habilidades: Habilidades[] = habilidades;
 
+  tecnologiaAtual:string = '';
+
+  // método informação exibida ao passar o mouse
+  atualizarTecnologia(tecnologia: string) :void{
+    this.tecnologiaAtual = tecnologia;
+  }
+
+  // método limpar informação sao remover o mouse
+  limparTecnologia():void{
+    this.tecnologiaAtual = '';
+  }
+
 }
