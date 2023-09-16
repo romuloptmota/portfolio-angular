53a4a5da2d359f434cfd40d760ea16e4b1bde66b
Romulo P T Mota <romuloptmota@gmail.com>
2023-09-16 00:16:31 -0300

    Criado funções de entrar e sair do modal box portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,7 +8,21 @@ import {Portfolio, portfolio} from "./portfolio";
 })
 export class PortfolioComponent {
 
+  // simulando banco de dados
   projetos: Portfolio[] = portfolio;
+
+  // exibir carrosel oculto com imagens pelo display
+  alterarDisplay: any  = {};
+
+  // funçao de ativar o carroucel oculto pelo display
+  abrirCarousel():void {
+    this.alterarDisplay = {display:'flex',};
+  }
+
+  // função de fechar carousel
+  fecharCarousel(): void {
+    this.alterarDisplay = {display:'none'}
+  }
 
 }
 
