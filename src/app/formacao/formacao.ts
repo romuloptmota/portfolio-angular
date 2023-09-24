2d3a3288c6224519a2cc3fe34f31eaf1c184f7e4
Romulo P T Mota <romuloptmota@gmail.com>
2023-09-24 20:38:11 -0300

    build correção de erros

--- src/app/formacao/formacao.ts
+++ src/app/formacao/formacao.ts
@@ -11,7 +11,7 @@ export const formacoesAcadenica = [
   {
     id: 1,
     link: "https://multivix.edu.br/",
-    logo: "assets/imagem/logo-instituicoes/multivix.jpg",
+    logo: "./assets/imagem/logo-instituicoes/multivix.jpg",
     nivel: "Pós",
     formacao: "Análise e desenvolvimento de sistema em tecnologia Python | Jan 2023 | Multivix",
     descricao: "Curso focado em aprimorar o conhecimento na linguagem estruturada utilizada no back-end de qualquer aplicação que demande escalabilidade, regras de negócio, lógica e afins.",
@@ -19,7 +19,7 @@ export const formacoesAcadenica = [
   {
     id: 2,
     link: "https://www.unopar.com.br/",
-    logo: "assets/imagem/logo-instituicoes/unopar.png",
+    logo: "./assets/imagem/logo-instituicoes/unopar.png",
     nivel: "Superior",
     formacao: "Análise e desenvolvimento de sistemas | Jul 2022 | Universidade do norte do Paraná",
     descricao: "Curso superior com direcionamento de carreira nas áreas de devops, mobile, back-end e front-end.",
@@ -27,7 +27,7 @@ export const formacoesAcadenica = [
   {
     id: 3,
     link: "https://startcapgemini.com.br/",
-    logo: "assets/imagem/logo-instituicoes/capgemini.png",
+    logo: "./assets/imagem/logo-instituicoes/capgemini.png",
     nivel: "Treinamento",
     formacao: "Programa START by Capgemini na trilha Java | Cursando 2023 | Capgemini",
     descricao: "Participando do programa Start By Capgemini, onde estou aprimorando todos os conhecimentos necessários em desenvolvimento web utilizando Angular e Java. ",
